6ff0260eb41b2a17fcd8c28277a38b3c24c92f0d
Max Schridde <maxjschridde@gmail.com>
2024-09-12 17:59:50 -0700

    fork ra-data-graphql-simple

---
+++ test-setup.js
@@ -0,0 +1,25 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+require('@testing-library/jest-dom');
+
+// Ignore warnings about act()
+// See https://github.com/testing-library/react-testing-library/issues/281,
+// https://github.com/facebook/react/issues/14769
+const originalError = console.error;
+jest.spyOn(console, 'error').mockImplementation((...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+        return;
+    }
+    originalError.call(console, ...args);
+});
+
+/**
+ * Mock fetch objects Response, Request and Headers
+ */
+const { Response, Headers, Request } = require('whatwg-fetch');
+
+global.Response = Response;
+global.Headers = Headers;
+global.Request = Request;
